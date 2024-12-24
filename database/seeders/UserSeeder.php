5482d0ce42885e37b4fef572716323367e099003
kakako222 <mikanmikan428@gmail.com>
2024-12-24 09:08:42 +0900

    edit.blade.phpのpatchをputに変更

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class UserSeeder extends Seeder
     {
         // 100件のダミーユーザーデータを生成
         User::factory()->count(100)->create();
+        $user = new User();
+        $user->name = "課題レビュー";
+        $user->kana = "カダイレビュー";
+        $user->email = "kadai@example.com";
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make('password');
+        $user->postal_code = "1234567";
+        $user->address = "東京都";
+        $user->phone_number = "85274597894";
+        $user->save();
     }
 }

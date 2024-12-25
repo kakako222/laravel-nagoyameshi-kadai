00afe2989d02a4712542f7110b7f2c2d098908a1
kakako222 <mikanmikan428@gmail.com>
2024-12-25 10:24:05 +0900

    会員側の店舗詳細ページを作ろう

--- tests/Feature/RestaurantTest.php
+++ tests/Feature/RestaurantTest.php
@@ -18,6 +18,7 @@ class RestaurantTest extends TestCase
 {
     use RefreshDatabase;
 
+    //////////index//////////
     /**
      * 未ログインのユーザーは会員側の店舗一覧ページにアクセスできる
      *
@@ -66,6 +67,68 @@ class RestaurantTest extends TestCase
 
         // 店舗一覧ページにアクセス
         $response = $this->withoutMiddleware()->actingAs($admin, 'admin')->get(route('restaurants.index'));
+        $response->assertStatus(302);
+
+        // リダイレクト先を確認
+        $response->assertRedirect(route('admin.home'));
+    }
+
+    //////////show//////////
+    /**
+     * 未ログインのユーザーは会員側の店舗詳細ページにアクセスできる
+     *
+     * @return void
+     */
+    public function test_unauthenticated_user_can_access_restaurant_show_page()
+    {
+        // 店舗データを作成
+        $restaurant = Restaurant::factory()->create();
+
+        // 未ログイン状態で店舗詳細ページにアクセス
+        $response = $this->get(route('restaurants.show', $restaurant));
+
+        // ステータスコード200（正常なアクセス）を確認
+        $response->assertStatus(200);
+    }
+    /**
+     * ログイン済みの一般ユーザーは会員側の店舗詳細ページにアクセスできる
+     *
+     * @return void
+     */
+    public function test_authenticated_user_can_access_restaurant_show_page()
+    {
+        // 一般ユーザーを作成してログイン
+        $user = User::factory()->create();
+
+        // 店舗データを作成
+        $restaurant = Restaurant::factory()->create();
+
+        // ログイン状態で店舗詳細ページにアクセス
+        $response = $this->actingAs($user)->get(route('restaurants.show', $restaurant));
+
+        // ステータスコード200（正常なアクセス）を確認
+        $response->assertStatus(200);
+    }
+    /**
+     * ログイン済みの管理者は会員側の店舗詳細ページにアクセスできない
+     *
+     * @return void
+     */
+    public function test_authenticated_admin_cannot_access_restaurant_show_page()
+    {
+        // 管理者ユーザーを作成してログイン
+        $admin = new Admin();
+        $admin->email = 'admin@example.com';
+        $admin->password = Hash::make('nagoyameshi');
+        $admin->save();
+
+        // 店舗データを作成
+        $restaurant = Restaurant::factory()->create();
+
+        // ログイン状態で管理者が店舗詳細ページにアクセス
+        $response = $this->actingAs($admin, 'admin')->get(route('restaurants.show', $restaurant));
+
+        // ステータスコード302（リダイレクト）を確認
         $response->assertStatus(302);
 
         // リダイレクト先を確認
